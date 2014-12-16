1d86206432896474a0ce4023bfd2d73dd452314d
Daniel Schröder <daniel.schroeder@gravitymedia.de>
2014-12-16 22:58:13 +0100

    Added missing peer module

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express'),
-  config = require('./config/config');
+  config = require('./config/config'),
+  peer = require('peer');
 
 var app = express();
 
 require('./config/express')(app, config);
 
 var server = app.listen(config.port),
-    options = require('./config/peer');
+  options = require('./config/peer');
 
 app.use('/peer', peer.ExpressPeerServer(server, options));
