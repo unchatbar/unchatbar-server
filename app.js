d58e02be0ddcc92eb06d1ab75dffb77dd7796ea6
Lars Wiedemann <Larswiedemann@gmx.net>
2015-01-21 21:32:47 +0100

    fix routing

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ var bodyParser = require('body-parser'),
   logger = require('morgan'),
   methodOverride = require('method-override'),
   peer = require('peer');
-
+console.log(config.peer);
 var app = express(),
   server = http.createServer(app),
   peerServer = peer.ExpressPeerServer(server, config.peer);
+/*
+routing bug
+-->http://[HOST]:8181/peerjs/PC/xdgerakrt8d/id?i=0
 
 app.set('views', config.root + '/app/views');
 app.set('view engine', 'jade');
@@ -31,7 +34,7 @@ glob.sync(config.root + '/app/controllers/*.js').forEach(function (controller) {
 });
 
 app.use('/peerjs', peerServer);
-
+/*
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -56,6 +59,6 @@ app.use(function (err, req, res) {
     error: {},
     title: 'error'
   });
-});
+});*/
 
 server.listen(config.port);
